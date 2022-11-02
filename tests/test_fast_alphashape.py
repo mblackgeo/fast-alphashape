9bd50810ae2b41d61fc859fa0d4674dae73bc4c6
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-11-02 11:11:40 +0000

    test: add python test

--- tests/test_fast_alphashape.py
+++ tests/test_fast_alphashape.py
@@ -1,5 +1,28 @@
-import fast_alphashape
+import numpy as np
+from fast_alphashape import __version__, alpha_shape
+from shapely import wkt
 
 
 def test_version():
-    assert fast_alphashape.__version__ is not None
+    assert __version__ is not None
+
+
+def test_basic_2d_shape():
+    points_2d = np.array(
+        [
+            (0.0, 0.0),
+            (0.0, 1.0),
+            (1.0, 1.0),
+            (1.0, 0.0),
+            (0.5, 0.25),
+            (0.5, 0.75),
+            (0.25, 0.5),
+            (0.75, 0.5),
+        ]
+    )
+    res = alpha_shape(points_2d, 2.0)
+    expect = wkt.loads(
+        "POLYGON ((0.75 0.5, 1 0, 0.5 0.25, 0 0, 0.25 0.5, 0 1, 0.5 0.75, 1 1, 0.75 0.5))"
+    )
+
+    assert res.equals(expect)
